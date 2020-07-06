9eb4089ea50ec1c3ebd09eeb3739a1e64804d7d8
vincent-peugnet <v.peugnet@free.fr>
2020-07-06 03:08:34 +0200

    add config file

--- index.php
+++ index.php
@@ -1,71 +1,11 @@
 <?php
 
-// Returns a file size limit in bytes based on the PHP upload_max_filesize
-// and post_max_size
-function file_upload_max_size()
-{
-    static $max_size = -1;
-  
-    if ($max_size < 0) {
-      // Start with post_max_size.
-        $post_max_size = parse_size(ini_get('post_max_size'));
-        if ($post_max_size > 0) {
-            $max_size = $post_max_size;
-        }
-  
-      // If upload_max_size is less, then reduce. Except if upload_max_size is
-      // zero, which indicates no limit.
-        $upload_max = parse_size(ini_get('upload_max_filesize'));
-        if ($upload_max > 0 && $upload_max < $max_size) {
-            $max_size = $upload_max;
-        }
-    }
-    return $max_size;
-}
-
-function parse_size($size)
-{
-    $unit = preg_replace('/[^bkmgtpezy]/i', '', $size); // Remove the non-unit characters from the size.
-    $size = preg_replace('/[^0-9\.]/', '', $size); // Remove the non-numeric characters from the size.
-    $size = floatval($size);
-    if ($unit) {
-      // Find the position of the unit in the ordered string which is the power of magnitude to multiply a kilobyte by.
-        return round($size * pow(1024, stripos('bkmgtpezy', $unit[0])));
-    } else {
-        return round($size);
-    }
-}
-
-
-function readablesize($bytes, $base = 2 ** 10)
-{
-    $format = ' %d %s';
-
-    if ($base === 2 ** 10) {
-        $i = 'i';
-    } else {
-        $i = '';
-    }
-
-    $unit = '';
-
-    if ($bytes < $base) {
-        $num = $bytes;
-    } elseif ($bytes < $base ** 2) {
-        $num = round($bytes / $base, 1);
-        $unit = 'K' . $i;
-    } elseif ($bytes < $base ** 3) {
-        $num = round($bytes / $base ** 2, 1);
-        $unit = 'M' . $i;
-    } else {
-        $num = round($bytes / $base ** 3, 1);
-        $unit = 'G' . $i;
-    }
-
-    return sprintf($format, $num, $unit);
-}
+require('fn.php');
 
 echo '<p>Max upload size : ' . readablesize(file_upload_max_size()) . 'o</p>';
+
+$conf = json_decode(file_get_contents("config.json"));
+
 
 ?>
 
@@ -73,8 +13,11 @@ echo '<p>Max upload size : ' . readablesize(file_upload_max_size()) . 'o</p>';
 
 
 <form enctype="multipart/form-data" method="post" action="upload.php">
-  <input type="file" name="files[]" multiple>
-  <input type="submit" name="Submit" value="upload">
+    <input type="file" name="files[]" multiple>
+    <?php if (!empty($conf->password)) { ?>
+        <input type="password" name="password" id="password" placeholder="password">
+    <?php } ?>
+    <input type="submit" name="Submit" value="upload">
 </form>
 
 
--- upload.php
+++ upload.php
@@ -5,53 +5,63 @@ echo '<a href=".">back</a></br>';
 require('./vendor/autoload.php');
 
 
+$conf = json_decode(file_get_contents("config.json"));
+
+if (
+    empty($conf->password)
+    ||
+    (
+        !empty($_POST['password'])
+        && $conf->password === $_POST['password']
+    )
+) {
 
 
+    // Simple validation (max file size 2MB )
+    $validator = new FileUpload\Validator\Simple($conf->maxuploadsize);
+
+    // Simple path resolver, where uploads will be put
+    $pathresolver = new FileUpload\PathResolver\Simple('test/');
+
+    $filenamegenerator = new FileUpload\FileNameGenerator\Slug();
+
+    // The machine's filesystem
+    $filesystem = new FileUpload\FileSystem\Simple();
+
+    // FileUploader itself
+    $fileupload = new FileUpload\FileUpload($_FILES['files'], $_SERVER);
+
+    // Adding it all together. Note that you can use multiple validators or none at all
+    $fileupload->setPathResolver($pathresolver);
+    $fileupload->setFileSystem($filesystem);
+    $fileupload->addValidator($validator);
+    $fileupload->setFileNameGenerator($filenamegenerator);
 
 
+    // Doing the deed
+    list($files, $headers) = $fileupload->processAll();
 
-
-
-// Simple validation (max file size 2MB )
-$validator = new FileUpload\Validator\Simple('2M');
-
-// Simple path resolver, where uploads will be put
-$pathresolver = new FileUpload\PathResolver\Simple('test/');
-
-$filenamegenerator = new FileUpload\FileNameGenerator\Slug();
-
-// The machine's filesystem
-$filesystem = new FileUpload\FileSystem\Simple();
-
-// FileUploader itself
-$fileupload = new FileUpload\FileUpload($_FILES['files'], $_SERVER);
-
-// Adding it all together. Note that you can use multiple validators or none at all
-$fileupload->setPathResolver($pathresolver);
-$fileupload->setFileSystem($filesystem);
-$fileupload->addValidator($validator);
-$fileupload->setFileNameGenerator($filenamegenerator);
-
-
-// Doing the deed
-list($files, $headers) = $fileupload->processAll();
-
-// Outputting it, for example like this
-foreach($headers as $header => $value) {
-    header($header . ': ' . $value);
-}
-
-var_dump(['files' => $files]);
-
-foreach($files as $file){
-    //Remeber to check if the upload was completed
-    if ($file->completed) {
-        echo $file->getRealPath();
-        
-        // Call any method on an SplFileInfo instance
-        var_dump($file->isFile());
+    // Outputting it, for example like this
+    foreach($headers as $header => $value) {
+        header($header . ': ' . $value);
     }
+
+    var_dump(['files' => $files]);
+
+    foreach($files as $file){
+        //Remeber to check if the upload was completed
+        if ($file->completed) {
+            echo $file->getRealPath();
+            
+            // Call any method on an SplFileInfo instance
+            var_dump($file->isFile());
+        }
+    }
+
+} else {
+    echo '<p>wrong password</p>';
 }
+
 
 ?>
 
